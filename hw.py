106013b57c4006527a117b61951b1d048ec9d949
zarapatel117 <zarapatel2510@gmail.com~>
2025-01-09 23:00:59 +0530

    hw

--- hw.py
+++ hw.py
@@ -1,13 +1,16 @@
 import math
 
 def calculate_circumference(radius):
-    
     if radius < 0:
-        return 
+        return None  
     return 2 * math.pi * radius
 
-radius = float(input("Enter the radius of the circle: "))
-circumference = calculate_circumference (radius)
-print("The circumference of the circle is: ",circumference)
-
-print("Please enter a valid number for the radius.")
+try:
+    radius = float(input("Enter the radius of the circle in decimal: "))
+    if radius < 0:
+        print("The radius cannot be negative. Please enter a valid radius.")
+    else:
+        circumference = calculate_circumference(radius)
+        print("The circumference of the circle is:" ,circumference)
+except ValueError:
+    print("Invalid number")
